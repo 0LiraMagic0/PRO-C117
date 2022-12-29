3fdf28aa60c9601ddb9ca675030480eff43a53bd
0LiraMagic0 <86988077+0LiraMagic0@users.noreply.github.com>
2022-12-29 17:18:59 -0500

    Add files via upload

---
+++ Project117/PRO-C105-Project-Images-main/Images/CreateVideo.py
@@ -0,0 +1,38 @@
+import file
+from itertools import count
+import os
+from turtle import width 
+import cv2
+from cv2 import VideoWriter
+import height
+
+path = "Images/"
+
+images = [ ]
+
+for i in images:
+    os.listdir(path)
+    os.splitext(file)
+
+    
+if ext in ['.gif','.png','.jpg','jpeg','jfif']:
+    file_name = path+"/"+file
+
+print(file_name)
+
+images.append()
+len(images)
+
+frame = cv2.imread(images[0])
+frame.shape(width,height)
+size = (width,height)
+
+print(size)
+
+out = cv2,VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'DIVX'),0.8,size)
+
+for i in range(0, count-1):
+    cv2.imread()
+
+out.write()
+print("Done")
